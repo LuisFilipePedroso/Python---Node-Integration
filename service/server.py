ded7b16cddcded08e11fe3ac9a94393ae66eb7fe
Luis Filipe <luis_filipe42@outlook.com>
2019-10-01 11:16:22 -0300

    Implementing the python loop to emit after 5sec

--- service/server.py
+++ service/server.py
@@ -1,10 +1,10 @@
 from aiohttp import web
 import socketio
+import asyncio
 
 sio = socketio.AsyncServer(cors_allowed_origins='*')
 app = web.Application()
 sio.attach(app)
-
 
 async def index(request):
     return web.Response(text="Hello World", content_type='text/html')
@@ -13,7 +13,24 @@ async def index(request):
 async def print_message(sid, message):
     await sio.emit('message', message)
 
+@sio.event
+async def connect(sid, environ):
+    print('User has connected')
+
 app.router.add_get('/', index)
+
+async def emitMessage():
+    while True:
+        await sio.emit('message', 'Luis')
+        await asyncio.sleep(5)
+
+@sio.event
+def emit(sid, environ):
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(emitMessage())
+    loop.run_forever()
 
 if __name__ == '__main__':
     web.run_app(app, port = 5000)
+    
+
