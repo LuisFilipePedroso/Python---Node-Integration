f7d1b58f69455fcb73fac5941f22a6fb8b559694
Luis Filipe <luis_filipe42@outlook.com>
2019-10-01 22:37:04 -0300

    Finished to create the python service and the integration HTML -> Node API

--- service/server.py
+++ service/server.py
@@ -10,6 +10,10 @@ sio.attach(app)
 arr = {}
 
 async def index(request):
+    f = open("data.txt", "r")
+    for message in f.readlines():
+        arr.update({'message': message})
+
     return web.json_response(arr)
 
 @sio.on('message')
@@ -19,16 +23,18 @@ async def print_message(sid, message):
 @sio.event
 async def connect(sid, environ):
     print('User has connected')
+    file = open("data.txt","w+")
+    file.close()
 
 app.router.add_get('/', index)
 
 async def emitMessage():
     while True:
-        await sio.emit('message', 'Luis')
-        number = random.randint(1, 500)
-        key = 'N' + str(number)
-        arr.update({key:number})
-        await asyncio.sleep(5)
+        f=open("data.txt", "a+")
+        f.write("Test "+str(random.randint(1, 500)) + "\n")
+        f.close()
+        await sio.emit('message', 'file')
+        await asyncio.sleep(20)
 
 @sio.event
 def emit(sid, environ):
